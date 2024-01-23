c6a11ef0497143f10c84e92e3e3d4369c489c94a
Minhee Lee <76970075+minigb@users.noreply.github.com>
2024-01-24 03:28:37 +0900

    Done with initial code. (#1)

---
+++ pick_setter.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+import datetime
+import random
+
+
+TODAY = datetime.date.today()
+START_DATE = '2023-07-04'
+
+
+def main():
+    info = pd.read_csv('info.csv')
+
+    bag = []
+    for i in range(len(info)):
+        if not info.loc[i, 'is_target']:
+            continue
+
+        handle = info.loc[i, 'handle']
+        last_date = info.loc[i, 'last_date']
+        if pd.isnull(last_date):
+            last_date = START_DATE
+        
+        weeks_passed = (pd.to_datetime(TODAY) - pd.to_datetime(last_date)).days // 7
+        bag.extend([handle] * weeks_passed)
+
+    if len(bag) == 0:
+        bag = list(info['handle'])
+
+    random.shuffle(bag)
+    random_pick = random.choice(bag)
+    print("Next setter:", random_pick)
+
+    
+if __name__ == '__main__':
+    main()
