f6e0ded43aaecf21195f9a7e27b61874388687cb
Minhee Lee <76970075+minigb@users.noreply.github.com>
2024-02-12 23:30:39 +0900

    Change to use json file instead of csv. (#4)

--- pick_setter.py
+++ pick_setter.py
@@ -1,14 +1,31 @@
 import pandas as pd
+import json
 import datetime
 import random
 import time
+import numpy as np
 
 TODAY = datetime.date.today()
 START_DATE = '2023-07-04'
 
 
+def read_info(file_name):
+    if file_name.endswith('.csv'):
+        return pd.read_csv(file_name)
+    elif file_name.endswith('.json'):
+        with open(file_name) as f:
+            info = json.load(f)
+            return pd.DataFrame.from_dict(info)
+
+
 def main():
-    info = pd.read_csv('info.csv')
+    info = read_info('info.json')
+    
+    # Fill in the start date for those who don't have it
+    info.loc[info['last_date'].isnull(), 'last_date'] = START_DATE
+    assert not info.isnull().values.any()
+
+    # If there's no target, set all non-army members as target
     if not info['is_target'].any():
         info.loc[info['is_in_army'] == False, 'is_target'] = True
 
@@ -16,20 +33,16 @@ def main():
 
     bag = []
     for i in range(len(info)):
+        # Check if the person is a target
         if not info.loc[i, 'is_target']:
             continue
         assert info.loc[i, 'is_in_army'] == False
-        
+
+        # Put the person in the bag with the weight of weeks passed
         id = info.loc[i, 'id']
         last_date = info.loc[i, 'last_date']
-        if pd.isnull(last_date):
-            last_date = START_DATE
-        
         weeks_passed = (pd.to_datetime(TODAY) - pd.to_datetime(last_date)).days // 7
         bag.extend([id] * weeks_passed)
-
-    if len(bag) == 0:
-        bag = list(info['id'])
 
     random.shuffle(bag)
     random_pick = random.choice(bag)
