ae4554beb4108f8cc6c91e13b80cc79f264d3bb4
Minhee Lee <76970075+minigb@users.noreply.github.com>
2024-02-27 16:43:53 +0900

    Refine code (#5)

--- pick_setter.py
+++ pick_setter.py
@@ -3,7 +3,6 @@ import json
 import datetime
 import random
 import time
-import numpy as np
 
 TODAY = datetime.date.today()
 START_DATE = '2023-07-04'
@@ -16,21 +15,29 @@ def read_info(file_name):
         with open(file_name) as f:
             info = json.load(f)
             return pd.DataFrame.from_dict(info)
+        
 
-
-def main():
-    info = read_info('info.json')
-    
+def refine_data(info):
     # Fill in the start date for those who don't have it
     info.loc[info['last_date'].isnull(), 'last_date'] = START_DATE
+    # Fill in 'is_in_army' for those who don't have it
+    info.loc[info['is_in_army'].isnull(), 'is_in_army'] = False
+    # Fill in 'is_target' for those with 'is_in_army' as True
+    info.loc[info['is_in_army'], 'is_target'] = False
+    # Check if there's any missing value
     assert not info.isnull().values.any()
+    # Check if last week's setter is updated
+    last_week_date = (TODAY - datetime.timedelta(days=7)).strftime('%Y-%m-%d')
+    assert last_week_date in set(info['last_date'])
 
     # If there's no target, set all non-army members as target
     if not info['is_target'].any():
         info.loc[info['is_in_army'] == False, 'is_target'] = True
 
-    print('Targets: ', list(info[info['is_target']]['id']))
+    return info
 
+
+def pick_setter(info):
     bag = []
     for i in range(len(info)):
         # Check if the person is a target
@@ -46,17 +53,22 @@ def main():
 
     random.shuffle(bag)
     random_pick = random.choice(bag)
-    
-    # Hold on for a while for suspense XD
-    for i in range(2):
+
+    return random_pick
+
+
+def give_suspense():
+    for _ in range(2):
         time.sleep(0.8)
         print('.')
     time.sleep(2)
-    
-    # Print result
-    print("Next setter:", random_pick, '\U0001F389') # print with emoji
-    time.sleep(1)
 
-    
+
 if __name__ == '__main__':
-    main()
+    info = read_info('info.json')
+    info = refine_data(info)
+    print('Targets: ', list(info[info['is_target']]['id']))
+
+    setter = pick_setter(info)
+    give_suspense()
+    print("Next setter:", setter, '\U0001F389') # print with emoji
